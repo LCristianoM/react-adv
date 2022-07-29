1603de5c8505f3094800bbde8e8b6d81dc734183
Leandro Cristiano <cristiano.g1889@gmail.com>
2022-07-29 14:41:37 -0500

    update

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -14,7 +14,6 @@ export const ShoppingPage = () => {
 			<ProductCard
 				key={product.id}
 				product={product}
-				className="bg-dark text-white"
 				initialValues={{
 					count: 0,
 					maxCount: 15,
@@ -22,19 +21,9 @@ export const ShoppingPage = () => {
 			>
 				{({ reset, isMaxCountReached, maxCount, increaseBy, count }) => (
 					<>
-						<ProductImage className="custom-image" style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.3)" }} />
-						<ProductTitle className="" />
-						<ProductButtons className="custom-buttons" />
-
-						{/* { JSON.stringify( args, null, 3)} */}
-						<button onClick={reset}> Reset </button>
-						<button onClick={() => increaseBy(-2)}> -2 </button>
-
-						{
-							(!isMaxCountReached && <button onClick={ () => increaseBy(+2)}> +2 </button>)
-						}
-
-						<span>{count} - {maxCount} </span>
+						<ProductImage />
+						<ProductTitle  />
+						<ProductButtons  />		
 					</>
 				)}
 			</ProductCard>
